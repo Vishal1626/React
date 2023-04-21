2c7b0bcfe542bc835eb4c70384cbffa8b57087c0
Vishal Jadhav <17vishaljadhav@gmail.com>
2023-04-21 17:49:33 +0530

    Chapter 2

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm an h1 Tag"),
+    React.createElement("h1", {}, "This is namste react"),
     React.createElement("h2", {}, "I'm an h2 Tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
